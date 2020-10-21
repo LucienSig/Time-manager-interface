34a4d479d2e94fae01df400d93804e39611f61b0
Lucien SIGAYRET <luciensigayret@gmail.com>
2020-10-21 13:54:43 +0200

    added clock controller

--- api/lib/api_web/controllers/clock_controller.ex
+++ api/lib/api_web/controllers/clock_controller.ex
@@ -3,20 +3,76 @@ defmodule ApiWeb.ClockController do
 
   alias Api.Time
   alias Api.Time.Clock
+  alias Api.Repo
+  alias Api.Accounts.User
+  import Ecto.Query
 
   action_fallback ApiWeb.FallbackController
 
-  def index(conn, _params) do
-    clocks = Time.list_clocks()
-    render(conn, "index.json", clocks: clocks)
+  def read(conn, %{"userID" => userID}) do
+    clock = Repo.get_by(Clock, [user: userID])
+
+    if clock == nil do
+      conn
+      |> put_status(404)
+      |> json(%{"errors" => "{'credentials': ['clock not found']}"})
+    else
+      conn
+      |> put_status(201)
+      |> json(%{
+        "time": clock.time,
+        "status": clock.status,
+        "user": clock.user
+      })
+    end
+
+    conn
+    |> put_status(401)
+    |> json(%{"id" => userID})
   end
 
-  def create(conn, %{"clock" => clock_params}) do
-    with {:ok, %Clock{} = clock} <- Time.create_clock(clock_params) do
+  def create(conn, %{"userID" => userID}) do
+    count = Repo.all(from u in "clocks", select: fragment("count(*)"))
+
+    id = List.first(count)
+    user_tmp = Repo.get_by(User, [id: userID])
+    clock = Repo.get_by(Clock, [user: userID])
+
+    if user_tmp do
+      if clock do
+        time = NaiveDateTime.utc_now()
+        status = !clock.status
+
+        changeset = Clock.changeset(clock, %{"time" => time, "status" => status, "user" => userID})
+        case Repo.update(changeset) do
+          {:ok, clock} ->
+          conn
+          |> put_status(200)
+          |> json(%{"Reponse" => "Updated", "time" => time, "status" => status, "user" => userID})
+          {:error, result} ->
+          conn
+          |> put_status(404)
+          |> json(%{"errors" => "{'credentials': ['no user found']}"})
+        end
+      else
+        status = true
+        date = NaiveDateTime.utc_now()
+        changeset = Clock.changeset(%Clock{}, %{"id" => id, "status" => status, "time" => date, "user" => userID})
+
+        case Repo.insert(changeset) do
+          {:ok, _user} ->
+            json(conn |> put_status(:created), %{success: "Created"})
+
+          {:error, changeset} ->
+            errors = "#{inspect(changeset.errors)}" |> String.replace("[", "(") |> String.replace("]", ")") |> String.replace("{", "[") |> String.replace("}", "]") |> String.replace("(", "{") |> String.replace(")", "}") |> String.replace(" :", " ")
+
+            json(conn |> put_status(:bad_request), %{errors: errors})
+        end
+      end
+    else
       conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.clock_path(conn, :show, clock))
-      |> render("show.json", clock: clock)
+      |> put_status(404)
+      |> json(%{"errors" => "{'credentials': ['no user found']}"})
     end
   end
 
