9bc2b4c094a0bbc17bb643246c0f2db41603ecb8
Lucien SIGAYRET <luciensigayret@gmail.com>
2020-10-22 13:26:33 +0200

    added email regex

--- api/lib/api/accounts/user.ex
+++ api/lib/api/accounts/user.ex
@@ -10,11 +10,13 @@ defmodule Api.Accounts.User do
     timestamps()
   end
 
+  @mail_regex ~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
   @doc false
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:email, :username, :is_active])
     |> validate_required([:email, :username, :is_active])
+    |> validate_format(:email, @mail_regex)
     |> unique_constraint(:email)
     |> unique_constraint(:username)
   end
