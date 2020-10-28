4cd8b2314bbf81ca8fe68672bb311340970865ce
Lucien SIGAYRET <luciensigayret@gmail.com>
2020-10-28 14:07:32 +0100

    fix

--- api/lib/api/accounts/user.ex
+++ api/lib/api/accounts/user.ex
@@ -1,14 +1,12 @@
 defmodule Api.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
 
   schema "users" do
     field :email, :string
     field :is_active, :boolean, default: false
     field :username, :string
     field :password, :string
-    field :password_full, :string, virtual: true
     field :role, :integer, default: 1
     field :team, :string
 
@@ -19,22 +17,11 @@ defmodule Api.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :username, :is_active, :role, :team, :password_full])
-    |> validate_required([:email, :username, :is_active, :role, :password_full])
+    |> cast(attrs, [:email, :username, :is_active, :role, :team, :password])
+    |> validate_required([:email, :username, :is_active, :role, :password])
     |> validate_format(:email, @mail_regex)
-    |> validate_length(:password_full, min: 8)
     |> unique_constraint(:email)
     |> unique_constraint(:username)
     |> put_password_hash
-  end
-
-  defp put_password_hash(changeset) do
-    case changeset do
-      %Ecto.Changeset{valid?: true, changes: %{password_full: pass}}
-        ->
-          put_change(changeset, :password, hashpwsalt(pass))
-      _ ->
-          changeset
-    end
   end
 end
--- api/mix.exs
+++ api/mix.exs
@@ -45,10 +45,7 @@ defmodule Api.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:cors_plug, "~> 2.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:comeonin, "~> 4.0"},
-      {:bcrypt_elixir, "~> 1.0"},
-      {:guardian, "~> 1.0"}
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 
